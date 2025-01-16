a3aeb5328c1929ee1dd165b230c4a42addc88f95
MG1350 <114371390+MG1350@users.noreply.github.com>
2025-01-16 14:30:58 -0500

    Create Lab.java

---
+++ Lab.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Lab{
+    public static void main(String[] args)
+    {
+        ArrayList<String> favorites = new ArrayList<String>();
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 6; i++)
+        {
+            System.out.println("What is your favorite color? ");
+            String userInput = input.nextLine();
+            favorites.add(userInput);
+        }
+        for (int i = 0; i < 6; i++)
+        {
+            System.out.println(favorites.get(i));
+        }
+    }
+}
